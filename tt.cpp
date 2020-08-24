c3ac99a0c631c28dbf1314a36cecc7f2cafab7ed
huweinan <huweinan@MacBook-Pro.local>
2020-08-24 21:24:23 +0800

    test hook

--- tt.cpp
+++ tt.cpp
@@ -1,26 +1,27 @@
 #include <iostream>
 
 class test {
-   public:
-    int a;
+public:
+  int a;
 
-    test() : a(111){};
+  test() : a(111){};
 
-    void fun(int c)
-    {
-        std::cout << "in test" << std::endl;
-        int b = 222;
-        // lambda decl
-        [c,b]() mutable {
-std::cout << c << std::endl;c += 1;
-        }();
-    };
+  void fun(int c) {
+    std::cout << "in test" << std::endl;
+    int b = 222;
+    // lambda decl
+    [c, b]() mutable {
+      std::cout << c << std::endl;
+      c += 1;
+    }();
+  };
 };
 
 int main() {
-    test t{};
-loop: t.fun(333);  //cout 111
-    t.fun(333);  //cout 112
-    goto loop;
-    return 0;
+  test t{};
+loop:
+  t.fun(333); // cout 111
+  t.fun(333); // cout 112
+  goto loop;
+  return 0;
 }
